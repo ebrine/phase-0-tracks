73d9fdf6744c0b50585aa8c694653f6d9a89363e
Eliza Brine <elizabrine@gmail.com>
2017-07-18 15:17:16 -0700

    adds intake questions for hamsters

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "What is the hamster's name?"
+hamster_name = gets.chomp
+
+puts "Estimate the volume level of the hamster (1-10 scale)"
+hamster_volume = gets.chomp
+
+puts "What color is the hamster's fur?"
+hamster_fur = gets.chomp
+
+puts "Is the hamster a good candidate for adoption? (y/n)"
+hamster_adoption_candidate = gets.chomp
+
+puts "Estimate the hamster's age"
+hamster_age = nil
+hamster_age = gets.chomp
