b571842c0f10b8a069c6f44fe22e4b3debee630a
Eliza Brine <elizabrine@gmail.com>
2017-07-21 12:39:36 -0700

    adds question for how many employees will be processed. while loop to process employees one by one

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,70 +1,83 @@
 # Ask new employee questions
 # store the answers in variables
 
-puts "What is your name?"
-employee_name = gets.chomp
+puts "How many employees will be processed?"
+  number_processed = gets.chomp.to_i
+  employee_number = 1;
 
-puts "How old are you?"
-employee_age = gets.chomp
-employee_age = employee_age.to_i
+  while employee_number <= number_processed
+    puts "What is your name?"
+    employee_name = gets.chomp
 
-puts "What year were you born?"
-employee_birthyear = gets.chomp
-employee_birthyear = employee_birthyear.to_i
+    puts "How old are you?"
+    employee_age = gets.chomp
+    employee_age = employee_age.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-employee_garlicbread = gets.chomp
-employee_garlicbread = employee_garlicbread.downcase
-# convert answer to true or false
-eatsgarlic = false
-if employee_garlicbread == "yes" || employee_garlicbread == "y"
-  eatsgarlic = true
-elsif employee_garlicbread == "no" || employee_garlicbread =="n"
-  eatsgarlic = false
-end
+    puts "What year were you born?"
+    employee_birthyear = gets.chomp
+    employee_birthyear = employee_birthyear.to_i
 
-puts "Would you like to enroll in the company's health insurance?"
-employee_insurance = gets.chomp
-employee_insurance = employee_insurance.downcase
-# convert answer to true or false
-wantsinsurance = false
-if employee_insurance == "yes" || employee_insurance == "y"
-  wantsinsurance = true
-elsif employee_insurance == "no" || employee_insurance=="n"
-  wantsinsurance = false
-end
+    puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+    employee_garlicbread = gets.chomp
+    employee_garlicbread = employee_garlicbread.downcase
+    # convert answer to true or false
+    eatsgarlic = false
+    if employee_garlicbread == "yes" || employee_garlicbread == "y"
+      eatsgarlic = true
+    elsif employee_garlicbread == "no" || employee_garlicbread =="n"
+      eatsgarlic = false
+    end
 
-## Logic Section
+    puts "Would you like to enroll in the company's health insurance?"
+    employee_insurance = gets.chomp
+    employee_insurance = employee_insurance.downcase
+    # convert answer to true or false
+    wantsinsurance = false
+    if employee_insurance == "yes" || employee_insurance == "y"
+      wantsinsurance = true
+    elsif employee_insurance == "no" || employee_insurance=="n"
+      wantsinsurance = false
+    end
 
-is_vampire = "Results Inconclusive"
+    ## Logic Section
 
-# Check whether employee got age correct
-# actual age is 2017 minus birthyear, or that number minus 1 if they haven't had a birthday yet
-calculated_age = 2017-employee_birthyear
+    is_vampire = "Results Inconclusive"
 
-# creates age_correct variable which stores whether the age math works out
-if (employee_age == calculated_age || employee_age == calculated_age-1 )
-  age_correct = true
-else
-  age_correct = false
-end
+    # Check whether employee got age correct
+    # actual age is 2017 minus birthyear, or that number minus 1 if they haven't had a birthday yet
+    calculated_age = 2017-employee_birthyear
 
-# FIRST CONDITION
-if age_correct == true && (eatsgarlic == true || wantsinsurance == true)
-  is_vampire = "Probably not a vampire."
-end
+    # creates age_correct variable which stores whether the age math works out
+    if (employee_age == calculated_age || employee_age == calculated_age-1 )
+      age_correct = true
+    elsif employee_age > 100
+      age_correct = false
+    else
+      age_correct = false
+    end
 
-# SECOND CONDITION
-if age_correct == false && (eatsgarlic == false || wantsinsurance == false)
-  is_vampire = "Probably a vampire."
-end
+    # FIRST CONDITION
+    if age_correct == true && (eatsgarlic == true || wantsinsurance == true)
+      is_vampire = "Probably not a vampire."
+    end
 
-# THIRD CONDITION
-if age_correct == false && eatsgarlic == false && wantsinsurance == false
-  is_vampire = "Almost certainly a vampire."
-end
+    # SECOND CONDITION
+    if age_correct == false && (eatsgarlic == false || wantsinsurance == false)
+      is_vampire = "Probably a vampire."
+    end
 
-if employee_name == "Drake Cula" || employee_name == "Tu Fang"
-  is_vampire = "Definitely a vampire."
-end
+    # THIRD CONDITION
+    if age_correct == false && eatsgarlic == false && wantsinsurance == false
+      is_vampire = "Almost certainly a vampire."
+    end
 
+    if employee_name == "Drake Cula" || employee_name == "Tu Fang"
+      is_vampire = "Definitely a vampire."
+    end
+
+    puts is_vampire
+    puts "Next Employee"
+    employee_number += 1
+  end
+
+exit
