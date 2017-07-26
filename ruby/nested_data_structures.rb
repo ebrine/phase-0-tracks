898b9720a07a2d6bccd709d04c2dacb4679b5c2f
Eliza Brine <elizabrine@gmail.com>
2017-07-26 15:04:23 -0700

    creates a nested data structure representing animals in a zoo

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,35 @@
+zoo = {
+  monkey: {
+    number: 6
+    names_ages: {
+      "Manny" => 12,
+      "Mickey" => 10,
+      "Maria" => 7,
+      "Mary-Ellen" => 2,
+      "Michel" => 5,
+      "Mace" => 6
+    }
+    food: ['bananas', 'melon', 'granola', 'grapes', 'peanuts']
+  }
+  sea_lion: {
+    number: 4
+    names_ages: {
+      'Sealy' => 3,
+      'Sami' => 4,
+      'Stella' => 2
+    }
+    food: ['cod', 'halibut', 'shrimp', 'kelp']
+  }
+  giraffe: {
+    number: 5
+    names_ages: {
+      'Geri' => 3,
+      'Geli' => 5,
+      'Jamie' => 6,
+      'Josie' => 2,
+      'Jaggy' => 7
+    }
+    food: ['fern', 'lettuce', 'natural leaves', 'corn nuts']
+  }
+
+}
