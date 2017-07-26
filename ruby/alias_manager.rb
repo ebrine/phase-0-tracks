97d694f7d7f2dad3e7bc87012f90550b1cd7f3c6
Eliza Brine <elizabrine@gmail.com>
2017-07-26 16:21:18 -0700

    adds alias method code to make it perform required functions

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -20,8 +20,40 @@ def alias_method(spy_name)
   spy_name_array[0], spy_name_array[1] = spy_name_array[1], spy_name_array[0]
   switched_spy_name = spy_name_array.join(' ')
 
-  
-
+  #lowercase all letters
+  switched_spy_name.downcase!
+  #break string into array of individual letters
+  spy_name_letters = switched_spy_name.chars
+  #create arrays of consonants and vowels
+  vowels = "aeiou".chars
+  consonants = "bcdfghjklmnpqrstvwxyz"
+  new_spy_name_letters = spy_name_letters.map do|letter|
+    is_vowel = vowels.include?(letter)
+    is_consonant = consonants.include?(letter)
+    if letter == " "
+      letter
+    elsif is_vowel == true
+      #puts "#{letter} is a vowel" #test code
+      if letter == "u"
+        #wrap around, replace with "a"
+        letter = "a"
+      end
+      #indexes the letter in the vowel array, gets the next vowel
+      new_vowel = vowels[vowels.index(letter) + 1]
+      letter = new_vowel
+    elsif is_consonant == true
+      #puts "#{letter} is a consonant" #test code
+      if letter == "z"
+        #wrap around, replace with "b"
+        letter = "b"
+      end
+      new_consonant = consonants[consonants.index(letter) + 1]
+      letter = new_consonant
+    end
+  end
+  new_spy_name = new_spy_name_letters.join
 end
 
 puts alias_method("eliza brine")
+puts alias_method("stefan DABroski")
+puts alias_method("Felicia Torres")
