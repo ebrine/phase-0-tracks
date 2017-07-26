192e3f12d74cf76c6d8b763bfe959533093a8153
Eliza Brine <elizabrine@gmail.com>
2017-07-26 13:37:15 -0700

    adds pseudocode for interior design form. starts asking questions and storing answers in hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,31 +1,93 @@
-design_app = {
-  "Name" =>  nil,
-  "Address" => nil,
-  "Email" => nil,
-  "Phone" => nil,
-  "Fave shade of blue" => nil,
-  "Wallpaper preferences" => nil,
-  "Ombre is" => nil
-}
+=begin
+  Pseudocode for interior designer/client hash
+
+1. create an empty hash called client info
+2. ask questions of the client
+3. if the user replies "none", skip to next question
+4 if the user enters an answer, convert answer to appropriate data type
+5. store key, value pair for every question answered
+by adding to the hash
+6. print hash back out on screen after all questions are asked
+6. ask user if they want to update an answer
+7. if yes, ask for which key they want to update
+8. ask for new answer
+9. convert key and value to appropriate data types and store in hash
+10.print out final hash. exit program
+=end
+
+
+
+client_info = {}
+puts "Please input the following information. If you do not wish to answer a question, simply enter \"none\"."
 
 puts "Name:"
-design_app["Name"] = gets.chomp
+client_name = gets.chomp
+if client_name == "none"
+else
+  client_info[:name] = client_name
+end
 
 puts "Address:"
-design_app["Address"] = gets.chomp
+client_address = gets.chomp
+if client_address == "none"
+else 
+  client_info[:address] = client_address
+end
 
-puts "Email:"
-design_app["Email"] = gets.chomp
+puts "Number of children:"
+client_children = gets.chomp
+if client_address == "none"
+else 
+  client_info[:children_number] = client_children.to_i
+end
 
-puts "Phone:"
-design_app["Phone"] = gets.chomp
-puts "Fave shade of blue:"
-design_app["Fave shade of blue"] = gets.chomp
+puts "Do you have cats or dogs? Yes or no:"
+client_has_pets = gets.chomp.downcase
+exit_flag = false
+until exit_flag == true
+  if client_has_pets == "none"
+    exit_flag = true
+  elsif client_has_pets == "yes"
+    client_info[:has_pets] = true
+    exit_flag = true
+  elsif client_has_pets == "no"
+    client_info[:has_pets] = false
+    exit_flag = true
+  else
+    puts "Do you have cats or dogs? Yes or no:"
+    client_has_pets = gets.chomp.downcase
+  end
+end
 
-puts "Wallpaper preferences:"
-design_app["Wallpaper preferences"] = gets.chomp
 
-puts "Ombre is:"
-design_app["Ombre is"] = gets.chomp
+puts "Urban, Suburban, or Rural:"
+client_city_type = gets.chomp
+if client_city_type == "none"
+else 
+  client_info[:city_type] = client_city_type
+end
 
-puts design_app
+
+puts "How many bedrooms in your house?"
+client_bedrooms = gets.chomp
+if client_bedrooms == "none"
+else 
+  client_info[:bedrooms] = client_bedrooms.to_i
+end
+
+puts "Please list the rooms you"
+client_bedrooms = gets.chomp
+if client_bedrooms == "none"
+else 
+  client_info[:bedrooms] = client_bedrooms.to_i
+end
+
+
+
+
+
+
+
+p client_info
+
+exit
