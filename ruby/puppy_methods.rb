99bceb930646365c1a1662d05c8360dc1791d605
Eliza Brine <elizabrine@gmail.com>
2017-08-02 09:29:39 -0700

    adds 5 methods to puppy class for release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times do
+      puts "woof"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(int)
+    age = 7 * int
+  end
+
+  def shake_hands
+    puts "*puppy shakes hands*"
+  end
 end
 
+## DRIVER CODE
+
+# puppy_new = Puppy.new
+# puppy_new.fetch("ball")
+# puppy_new.speak(5)
+# puppy_new.roll_over
+# p puppy_new.dog_years(5)
+# puppy_new.shake_hands
