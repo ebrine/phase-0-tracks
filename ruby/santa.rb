7c2ca3a204102733ef4098d53731bddcf6e847c7
Eliza Brine <elizabrine@gmail.com>
2017-08-02 12:46:31 -0700

    Refactors code with attr_reader and attr_accessor

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,45 +1,32 @@
 class Santa
+  attr_reader :age, :ethnicity
+  attr_accessor :gender
 
-def initialize(gender, ethnicity)
-  puts "Initializing Santa instance ..."
-  @gender = gender
-  @ethnicity = ethnicity
-  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer",
-    "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-  @age = 0
-end
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer",
+      "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
 
-def eat_milk_and_cookies(cookie)
-  puts "That was a good #{cookie}!"
-end
+  def eat_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}!"
+  end
 
-def speak
-  puts "Ho, ho, ho! Haaaappy holidays!" 
-end
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!" 
+  end
 
-def celebrate_birthday
-  @age += 1
-end
+  def celebrate_birthday
+    @age += 1
+  end
 
-def get_mad_at(reindeer)
-  @reindeer_ranking.delete(reindeer)
-  @reindeer_ranking << reindeer
-end
-
-# Setter method for gender
-def gender=(new_gender)
-  @gender = new_gender
-end
-
-# Getter method for age
-def age
-  @age
-end
-
-#Getter method for ethnicity
-def ethnicity
-  @ethnicity
-end
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking << reindeer
+  end
 
 
 end
