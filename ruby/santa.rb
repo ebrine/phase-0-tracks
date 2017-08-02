2f11e8a72ec8c840523fb1d0b2f1334865794329
Eliza Brine <elizabrine@gmail.com>
2017-08-02 12:35:35 -0700

    Adds celebrate birthday and get mad at methods, as well as gender setter method and age and ethnicity getter methods. adds driver code to test all new methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,6 +17,30 @@ def speak
   puts "Ho, ho, ho! Haaaappy holidays!" 
 end
 
+def celebrate_birthday
+  @age += 1
+end
+
+def get_mad_at(reindeer)
+  @reindeer_ranking.delete(reindeer)
+  @reindeer_ranking << reindeer
+end
+
+# Setter method for gender
+def gender=(new_gender)
+  @gender = new_gender
+end
+
+# Getter method for age
+def age
+  @age
+end
+
+#Getter method for ethnicity
+def ethnicity
+  @ethnicity
+end
+
 
 end
 
@@ -34,4 +58,10 @@ genders.each_index {|i|
   santas << Santa.new(genders[i], ethnicities[i])
 }
 
-p santas
+#p santas
+
+puts santa1.get_mad_at("Vixen")
+puts santa1.celebrate_birthday
+puts santa1.gender = "fluid"
+puts santa1.age
+puts santa1.ethnicity
