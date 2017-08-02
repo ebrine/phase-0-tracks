1d574e42871f8ca4e8a18c63b31b12ccc20cf4a9
Eliza Brine <elizabrine@gmail.com>
2017-08-02 11:57:53 -0700

    creates santa class and initialize, speak and eat milk and cookies mehtods

---
+++ ruby/santa.rb
@@ -0,0 +1,17 @@
+class Santa
+
+def initialize
+  puts "Initializing Santa instance ..."
+end
+
+def eat_milk_and_cookies(cookie)
+  puts "That was a good #{cookie}"
+end
+
+def speak
+  puts "Ho, ho, ho! Haaaappy holidays!" 
+end
+
+
+end
+
