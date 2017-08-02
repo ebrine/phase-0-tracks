db75e246e3587aae2918f3ac2c42455fff41e8a4
Eliza Brine <elizabrine@gmail.com>
2017-08-02 12:18:14 -0700

    Completes Release 1, creating several different santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,7 +4,7 @@ def initialize(gender, ethnicity)
   puts "Initializing Santa instance ..."
   @gender = gender
   @ethnicity = ethnicity
-  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer"
+  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer",
     "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   @age = 0
 end
@@ -20,6 +20,18 @@ end
 
 end
 
-santa1 = Santa.new
+santa1 = Santa.new("Girl", "White")
 santa1.eat_milk_and_cookies("chocolate chip cookie")
 santa1.speak
+
+santas = []
+
+genders = ["male", "girl", "boy", "femme", "n/a"]
+ethnicities = ["Chinese", "Latina", "Latino", "Pirate", 
+"African"]
+
+genders.each_index {|i|
+  santas << Santa.new(genders[i], ethnicities[i])
+}
+
+p santas
