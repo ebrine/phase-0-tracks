928b3e5bf14d568f1da6500d9511df2fc4d5f0fe
Eliza Brine <elizabrine@gmail.com>
2017-08-02 09:52:03 -0700

    creates a boat class and adds a change boat type, sail around the lake and board boat method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -30,9 +30,45 @@ end
 
 ## DRIVER CODE
 
-puppy_new = Puppy.new
+# puppy_new = Puppy.new
 # puppy_new.fetch("ball")
 # puppy_new.speak(5)
 # puppy_new.roll_over
 # p puppy_new.dog_years(5)
 # puppy_new.shake_hands
+
+
+class Boat
+
+  def initialize(boat_type, color)
+    @boat_type = boat_type
+    @color = color
+    puts "Initializing a new #{color} #{boat_type}"
+  end
+
+  def change_boat_type=(new_boat_type)
+    @boat_type = new_boat_type
+    puts "Your boat is now a #{@boat_type}"
+  end
+
+  def boat
+    @boat_type
+  end
+
+  def board_boat(int)
+    puts "#{int} people have boarded the boat"
+  end
+
+  def sail_around_lake(number)
+    number.times do 
+    puts "You've sailed around the lake!"
+    end    
+  end
+end
+
+
+sailboat = Boat.new('power boat', 'blue')
+sailboat.change_boat_type = "sailboat"
+sailboat.boat
+sailboat.board_boat(5)
+sailboat.sail_around_lake(3)
