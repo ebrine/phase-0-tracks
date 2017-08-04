7cd219df08cfc5727075a7ab91e44b969539fa31
Eliza Brine <elizabrine@gmail.com>
2017-08-04 10:20:54 -0700

    Adds test for initialization method of WordGame

---
+++ ruby/word_game/word_game_spec.rb
@@ -0,0 +1,27 @@
+# WordGame class should be able to 
+
+
+
+
+
+require_relative 'word_game'
+
+
+describe WordGame do 
+
+  let (:game) {WordGame.new("orange")}
+
+  it "returns blank word upon initialization" do
+    expect(WordGame.new("orange")).to eq "------"
+  end
+
+  it "" do
+  end
+  
+  it "" do
+  end
+  
+  it "" do
+  end
+  
+end
