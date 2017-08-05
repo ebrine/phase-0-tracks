c2fb57ee410372654f51e51186cde6f8045ddf88
Eliza Brine <elizabrine@gmail.com>
2017-08-04 17:17:21 -0700

    Cleans up comments, puts pseudocode at top of document'

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -2,7 +2,6 @@
 
 ## Extra stuff I did:
 # Hid the player 1 input
-##ADD FUNCTIONALITY TO REJECT IF NOT A LETTER (EG ;, ',', ETC) or CAPS
 
 ### Game Class methods:
 
@@ -15,29 +14,7 @@
     # length, to be used as current status variable
   # letters guessed variable
   # outputs: prints blank word.
-require 'io/console'
 
-class WordGame
-  # add read/write vars here
-  # attr_reader: 
-  attr_accessor :current_word, :guess_count, :hidden_word,
-  :letters_guessed, :guesses_left, :game_over, :game_won
-
-  def initialize(hidden_word)
-    @hidden_word = hidden_word.split('')
-    @max_guesses = hidden_word.length * 2
-    @game_over = false
-    # REFACTOR??
-    @current_word = Array.new(hidden_word.length) { |i| "_" }
-    @guess_count = 0
-    @letters_guessed = []
-    @guesses_left = @max_guesses - @guess_count
-    @game_won = false
-  end
-
-
-## Test code
-# game = WordGame.new("orange")
 
 # guess_tracker
 # purpose: keeping track of the number of letters guessed
@@ -51,6 +28,43 @@ class WordGame
       # if letters guessed reaches max guesses
         # set game over variable to true
 # outputs: 
+
+# letter_checker
+# purpose: checking if letter is in word
+# inputs: letter
+  # check if letter is in word array
+
+   # if letter is not in word:
+    # return FALSE
+   # if letter is in secret word
+    # return [letter, index]
+
+# word_updater
+# purpose: updating current status of secret word
+# inputs: [letter, index] array from previous method
+  # take [letter, index] and update current word variable
+  # if all "-" have been replaced by letters
+    # set game_over variable to true
+
+
+require 'io/console'
+
+class WordGame
+
+  attr_accessor :current_word, :guess_count, :hidden_word,
+  :letters_guessed, :guesses_left, :game_over, :game_won
+
+  def initialize(hidden_word)
+    @hidden_word = hidden_word.split('')
+    @max_guesses = hidden_word.length * 2
+    @game_over = false
+    @current_word = Array.new(hidden_word.length) { |i| "_" }
+    @guess_count = 0
+    @letters_guessed = []
+    @guesses_left = @max_guesses - @guess_count
+    @game_won = false
+  end
+
   def guess_tracker(guessed_letter)
     if @letters_guessed.include?guessed_letter
       return nil
@@ -63,18 +77,6 @@ class WordGame
     return true
   end
 
-
-
-# letter_checker
-# purpose: checking if letter is in word
-# inputs: letter
-  # check if letter is in word array
-
-   # if letter is not in word:
-    # return FALSE
-   # if letter is in secret word
-    # return [letter, index]
-
   def letter_checker(guessed_letter)
     if @hidden_word.include?guessed_letter
       letter_index_array = @hidden_word.map.with_index{|x, i| i if x == guessed_letter}.compact
@@ -83,13 +85,6 @@ class WordGame
       return false
     end
   end
-
-# word_updater
-# purpose: updating current status of secret word
-# inputs: [letter, index] array from previous method
-  # take [letter, index] and update current word variable
-  # if all "-" have been replaced by letters
-    # set game_over variable to true
 
   def word_updater(array)
     correct_letter = array[0]
@@ -124,48 +119,9 @@ class WordGame
 
 end
 
-# game = WordGame.new("orange")
-# p game.guesses_left
-# game.guess_tracker("a")
-# game.guess_tracker("a")
-# # p game.guess_count
-# # p game.letters_guessed
-# # p game.guesses_left
-# p game.guess_tracker("o")
-# p game.guess_tracker("r")
-# p game.guess_tracker("a")
-# p game.guess_tracker("n")
-# p game.guess_tracker("g")
-# p game.guess_tracker("e")
-# p game.guess_count
-# p game.letter_checker("o")
-# p game.letter_checker("r")
-# p game.letter_checker("a")
-# p game.letter_checker("n")
-# p game.letter_checker("g")
-# p game.letter_checker("e")
-# game.word_updater(game.letter_checker("e"))
-# p game.current_word
-# game.word_updater(game.letter_checker("a"))
-# p game.current_word
-# game.word_updater(game.letter_checker("n"))
-# p game.current_word
-# game.word_updater(game.letter_checker("o"))
-# p game.current_word
-# game.word_updater(game.letter_checker("r"))
-# game.print_current_array
-# game.word_updater(game.letter_checker("g"))
-# game.word_updater(game.letter_checker("e"))
-# game.word_updater(game.letter_checker("e"))
-
-# p game.current_word
-# p game.current_word.include? "-"
-# p game.game_over
-# puts game.current_word
-# game.print_current_array
-
 
 ### User interface: && Driver Code
+
 # asks player 1 for word to hide
 # initializes game with inputed word
 # print @current_word
@@ -185,7 +141,7 @@ end
 # prints current status of secret word
 # prints guess count/ guesses left
 
-# After the loop exits (game is over)
+# Prints messages for success/failure at end of game 
 
 puts "Welcome to the Word Game!"
 puts "Player 1, please enter a word to hide:"
@@ -219,6 +175,4 @@ else
   puts " "
   puts game.hidden_word.join(" ")
 end
-
-
 
