a6fd898d5c4592978cc92353e0c81d69f19fd7ff
Eliza Brine <elizabrine@gmail.com>
2017-08-13 12:23:40 -0700

    Creates stringArray generator function. Adds pseudocode for function, and function itself. Adds driver code to create 10 string arrays and feed them to longestWord function, printing arrays and longest word

--- js/algos.js
+++ js/algos.js
@@ -1,6 +1,6 @@
 // Release 0 Pseudocode
 
-// Find the longest phrase
+// Find the longest phrase:
 // function should take an array
 // iterate through the array, 
 // taking the length of each entry
@@ -18,7 +18,7 @@
 // Release 1 Pseudocode
 
 // Find a Key-Value match:
-// Method takes two objects
+// Function takes two objects
 // iterate through the keys of the first object, 
 // and look to see if a match exists in the second object
 // If a match does exist,
@@ -27,12 +27,23 @@
 // If no key-value pair match, return false
 
 
+////////////////////////////////////////////////
+// Release 2 Pseudocode
+
+// Generate Random Test Data:
+// Function takes an integer (n)
+// creates an empty array
+// Do the following (n) times:
+// generate a random number between 1 and 10
+// create a string of that many letters
+// add the string to the array
+// At the end, return the array of (n) strings
+
+
 function longestWord(array) {
-  word_array = array;
-  longest = "";
+  var word_array = array;
+  var longest = "";
   for (var i = 0; i < word_array.length; i++) {
-    // console.log(word_array[i].length);
-    // console.log(longestWord.length);
     if (word_array[i].length > longest.length){
     longest = word_array[i];}
   }
@@ -40,11 +51,11 @@ function longestWord(array) {
 }
 
 function compareObjects(obj1, obj2){
-  matchExists = false;
-  keys1 = Object.keys(obj1);
-  keys2 = Object.keys(obj2);
+  var matchExists = false;
+  var keys1 = Object.keys(obj1);
+  var keys2 = Object.keys(obj2);
   for (var i in keys1){
-    key = keys1[i];
+    var key = keys1[i];
     if (keys2.indexOf(key) != -1) {
       if (obj1[key] == obj2[key]){
         matchExists = true;
@@ -54,10 +65,42 @@ function compareObjects(obj1, obj2){
   return matchExists;
 }
 
+function getRandom(){
+  // gives random number between 1 and 10 inclusive
+  return Math.floor(Math.random() * 9) + 1;
+}
+
+function stringArray(integer) {
+  var array = [];
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  for (var i = 1; i <= integer; i++) {
+    var num = getRandom();
+    var word = "";
+    for (var n = 1; n <= num; n++){
+      var rand = alphabet[Math.floor(Math.random() * alphabet.length)];
+      word += rand;
+    }
+    array.push(word);
+  }
+  return array;
+}
+
 // /// Driver Code
 // console.log(longestWord(["eliza", "lily", "madison", "angus"]));
 // console.log(longestWord(["long phrase","longest phrase","longer phrase"]));
 // console.log(longestWord(["giraffe", "sea lion", "elephant"]));
-console.log(compareObjects({name: "Steven", age: 24, dog: "Ruby"}, {name: "Tamir", age: 54, cat: "Fritz"})); // returns false
-console.log(compareObjects({name: "Steven", age: 54}, {name: "Tamir", age: 54})); // returns true
-console.log(compareObjects({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3})); // returns true
+
+// console.log(compareObjects({name: "Steven", age: 24, dog: "Ruby"}, {name: "Tamir", age: 54, cat: "Fritz"})); // returns false
+// console.log(compareObjects({name: "Steven", age: 54}, {name: "Tamir", age: 54})); // returns true
+// console.log(compareObjects({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3})); // returns true
+
+for (var i = 1; i <= 10; i++){
+  var arr = stringArray(getRandom());
+  console.log(arr);
+  var longest = longestWord(arr);
+  console.log(longest);
+}
+
+
+
+
