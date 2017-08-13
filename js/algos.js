36e484baad7b3bf4d604bf2b71b7d979bfc47aab
Eliza Brine <elizabrine@gmail.com>
2017-08-13 11:49:11 -0700

    Adds pseudocode for a function that compares key-value pairs of two objects. Adds code for function. Adds driver code to demonstrate

--- js/algos.js
+++ js/algos.js
@@ -10,6 +10,23 @@
 // that entry should be stored as longestWord
 // return longestWord when all items in array have been checked
 
+// known problem : doesn't deal with case when 2 words have equal length
+// only returns first one of that length to appear
+
+
+///////////////////////////////////////////////
+// Release 1 Pseudocode
+
+// Find a Key-Value match:
+// Method takes two objects
+// iterate through the keys of the first object, 
+// and look to see if a match exists in the second object
+// If a match does exist,
+// compare the values of those keys in both objects
+// If the values match, return true
+// If no key-value pair match, return false
+
+
 function longestWord(array) {
   word_array = array;
   longest = "";
@@ -22,10 +39,25 @@ function longestWord(array) {
   return longest
 }
 
-/// Driver Code
-console.log(longestWord(["eliza", "lily", "madison", "angus"]));
-console.log(longestWord(["long phrase","longest phrase","longer phrase"]));
-console.log(longestWord(["giraffe", "sea lion", "elephant"]));
+function compareObjects(obj1, obj2){
+  matchExists = false;
+  keys1 = Object.keys(obj1);
+  keys2 = Object.keys(obj2);
+  for (var i in keys1){
+    key = keys1[i];
+    if (keys2.indexOf(key) != -1) {
+      if (obj1[key] == obj2[key]){
+        matchExists = true;
+      }
+    }  
+  }
+  return matchExists;
+}
 
-// known problem : doesn't deal with case when 2 words have equal length
-// only returns first one of that length to appear
+// /// Driver Code
+// console.log(longestWord(["eliza", "lily", "madison", "angus"]));
+// console.log(longestWord(["long phrase","longest phrase","longer phrase"]));
+// console.log(longestWord(["giraffe", "sea lion", "elephant"]));
+console.log(compareObjects({name: "Steven", age: 24, dog: "Ruby"}, {name: "Tamir", age: 54, cat: "Fritz"})); // returns false
+console.log(compareObjects({name: "Steven", age: 54}, {name: "Tamir", age: 54})); // returns true
+console.log(compareObjects({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3})); // returns true
